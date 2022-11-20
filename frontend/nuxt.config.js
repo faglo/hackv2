8025550728ad3d2eadfd9370c87a658b9d703acb
Daniil Rakityansky <daniilrakityansky@Daniils-MacBook-Pro.local>
2022-11-20 14:31:08 +0300

    hz

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -3,82 +3,77 @@ export default {
   ssr: false,
   server: {
     port: 3000, // default: 3000
-    host: '0.0.0.0', // default: localhost,
-    timing: false
+    host: "0.0.0.0", // default: localhost,
+    timing: false,
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'frontend',
+    title: "frontend",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     // '~/assets/scss/_vars.scss',
-    '~/assets/scss/global.scss',
-    '~/assets/scss/_vars.scss',
+    "~/assets/scss/global.scss",
+    "~/assets/scss/_vars.scss",
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '@/plugins/swipe-modal.js', mode: 'client' },
+    { src: "@/plugins/swipe-modal.js", mode: "client" },
     { src: "~/plugins/ymapPlugin", mode: "client" },
-    '~/plugins/eventBus.js' ,
+    "~/plugins/eventBus.js",
   ],
   styleResources: {
-    scss: ["./assets/scss/*.scss"]
+    scss: ["./assets/scss/*.scss"],
   },
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  
-  buildModules: [
-    '@nuxtjs/composition-api/module'
-  ],
+
+  buildModules: ["@nuxtjs/composition-api/module"],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     [
       "vue-yandex-maps/nuxt",
       {
         apiKey: "ca913e0d-396f-41fb-aba4-8537720869c5",
         lang: "ru_RU",
-        version: "2.1"
-      }
+        version: "2.1",
+      },
     ],
-    
-    '@nuxtjs/style-resources',
+
+    "@nuxtjs/style-resources",
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
+    "@nuxtjs/pwa",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: "/",
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
-    }
+      lang: "en",
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
